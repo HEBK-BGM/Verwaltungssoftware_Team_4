2df4b61a724defa24779596375cc455b64347cbf
MrMajesrto <94058264+MrMajesrto@users.noreply.github.com>
2022-02-16 17:13:00 +0100

    Update class Cards
    wallah

--- Java/src/Schuelerverwaltung/src/Cards.java
+++ Java/src/Schuelerverwaltung/src/Cards.java
@@ -1,35 +1,38 @@
 public class Cards {
-    
+
+    public static void main(String[] args) throws Exception {
+
+    //Infos for Pokémon cards
     int id = 001;
     String name = "exapmlepokemon";
     String rarity = "non rare";
-    double price;
+    double price = 0.00;
     
-    //Keine Ahnung was falsch
-    if(rarity.equals = "non rare"){
-        price = 1.00;
+    //price set for each rarity
+    if(rarity.equals("non rare")){
+        price = price + 1.00;
     }
     else 
-    if(rarity.equals = "rare"){
-        price = 5.00;        
+    if(rarity.equals("rare")){
+        price = price +  5.00;        
     }
     else
-    if(rarity.equals = "super rare"){
-        price = 10.00;
+    if(rarity.equals("super rare")){
+        price = price +  10.00;
     }
     else 
-    if(rarity.equals = "extrem rare"){
-        price = 25.00;
+    if(rarity.equals("extrem rare")){
+        price = price +  25.00;
     }
     else 
-    if(rarity.equals = "pogO"){
-        price = 420.69;
+    if(rarity.equals("pogO")){
+        price = price +  420.69;
     }
     
     
     String[] pokemoninfo = {String.valueOf(id), name, rarity, String.valueOf(price) };
 
-    System.out.println(pokemoninfo[3]);
-    
+    //System.out.println(pokemoninfo[3]);
 
+    }
 }
