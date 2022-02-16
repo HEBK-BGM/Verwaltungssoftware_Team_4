8b7c69dcb3cd580bca31736ca38489c422626096
MrMajestro <manuel.bauland@hebk.de>
2022-02-16 15:37:34 +0100

    [FIX] Es funktioniert

--- Java/src/Schuelerverwaltung/src/Cards.java
+++ Java/src/Schuelerverwaltung/src/Cards.java
@@ -1,35 +1,39 @@
 public class Cards {
+
+    public static void main(String[] args) throws Exception {
     
     int id = 001;
     String name = "exapmlepokemon";
     String rarity = "non rare";
-    double price;
+    double price = 0.00;
     
 
-    if(rarity.equals = "non rare"){
-        price = 1.00;
+    if(rarity.equals("non rare")){
+        price = price + 1.00;
     }
     else 
-    if(rarity.equals = "rare"){
-    price = 5.00;        
+    if(rarity.equals("rare")){
+        price = price +  5.00;        
     }
     else
-    if(rarity.equals = "super rare"){
-        price = 10.00;
+    if(rarity.equals("super rare")){
+        price = price +  10.00;
     }
     else 
-    if(rarity.equals = "extrem rare"){
-        price = 25.00;
+    if(rarity.equals("extrem rare")){
+        price = price +  25.00;
     }
     else 
-    if(rarity.equals = "pogO"){
-        price = 420.69;
+    if(rarity.equals("pogO")){
+        price = price +  420.69;
     }
     
     
     String[] pokemoninfo = {String.valueOf(id), name, rarity, String.valueOf(price) };
 
     System.out.println(pokemoninfo[3]);
+
+    }
     
 
 }
