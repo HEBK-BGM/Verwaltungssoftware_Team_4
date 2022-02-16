eb585a53f0c87cdb1d66f7e4217425f62b94b5d6
Felix Wienands <felix.wienands@hebk.de>
2022-02-16 17:24:19 +0100

    formatierung verbessert

--- Java/src/Schuelerverwaltung/src/Cards.java
+++ Java/src/Schuelerverwaltung/src/Cards.java
@@ -2,37 +2,28 @@ public class Cards {
 
     public static void main(String[] args) throws Exception {
 
-    //Infos for Pokémon cards
-    int id = 001;
-    String name = "exapmlepokemon";
-    String rarity = "non rare";
-    double price = 0.00;
-    
-    //price set for each rarity
-    if(rarity.equals("non rare")){
-        price = price + 1.00;
-    }
-    else 
-    if(rarity.equals("rare")){
-        price = price +  5.00;        
-    }
-    else
-    if(rarity.equals("super rare")){
-        price = price +  10.00;
-    }
-    else 
-    if(rarity.equals("extrem rare")){
-        price = price +  25.00;
-    }
-    else 
-    if(rarity.equals("pogO")){
-        price = price +  420.69;
-    }
-    
-    
-    String[] pokemoninfo = {String.valueOf(id), name, rarity, String.valueOf(price) };
+        // Infos for Pokémon cards
+        int id = 001;
+        String name = "exapmlepokemon";
+        String rarity = "non rare";
+        double price = 0.00;
 
-    //System.out.println(pokemoninfo[3]);
+        // price set for each rarity
+        if (rarity.equals("non rare")) {
+            price = price + 1.00;
+        } else if (rarity.equals("rare")) {
+            price = price + 5.00;
+        } else if (rarity.equals("super rare")) {
+            price = price + 10.00;
+        } else if (rarity.equals("extrem rare")) {
+            price = price + 25.00;
+        } else if (rarity.equals("pogO")) {
+            price = price + 420.69;
+        }
+
+        String[] pokemoninfo = { String.valueOf(id), name, rarity, String.valueOf(price) };
+
+        // System.out.println(pokemoninfo[3]);
 
     }
 }
