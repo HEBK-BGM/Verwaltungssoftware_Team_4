3c5c4e7ef8223761dc7ffee8d8a439d51595705e
Mr.Majestro <manuel.bauland@hebk.de>
2022-03-03 17:18:44 +0100

    [ADD] Beschreibung des Menues

---
+++ Java/src/Schuelerverwaltung/src/Menu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+
+public class Menu {
+
+
+    Scanner sc = new Scanner(System.in);
+
+    public void breakLine(){
+        System.out.println("------------------");
+    }
+
+
+    public void start(){
+        breakLine();
+        System.out.println("Willkommen zu unserem Card management Programm");
+        breakLine();
+    }
+
+    public User createUser(){
+        
+    }
+    
+}
